031d02d77b1448f37c01a06450da30e0835c9fbb
Tim <tima2508@yandex.ru>
2023-08-21 18:45:44 +0300

    selection sort

--- problems/selection-sort/selection-sort.js
+++ problems/selection-sort/selection-sort.js
@@ -2,6 +2,17 @@
 // https://en.wikipedia.org/wiki/Selection_sort
 
 export function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let curMinInd = i;
 
-    return arr;
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[curMinInd]) {
+        curMinInd = j;
+      }
+    }
+
+    [arr[i], arr[curMinInd]] = [arr[curMinInd], arr[i]]
+  }
+
+  return arr;
 }
