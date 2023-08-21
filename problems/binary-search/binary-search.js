b62085bbb4123b11b62b3940a000ca0cb08f3231
Tim <tima2508@yandex.ru>
2023-08-21 20:31:41 +0300

    binary-search

--- problems/binary-search/binary-search.js
+++ problems/binary-search/binary-search.js
@@ -1,5 +1,20 @@
 // https://en.wikipedia.org/wiki/Binary_search_algorithm
 
 export function binarySearch(arr, value) {
+  let start = 0;
+  let end = arr.length - 1;
 
+  while (start <= end) {
+    let mid = (start + end) >> 1;
+
+    if (arr[mid] < value) {
+      start = mid + 1
+    } else if (arr[mid] > value) {
+      end = mid - 1
+    } else {
+      return mid
+    }
+  }
+
+  return undefined;
 }
