7fdcbff65296d221ddeb74e3f29b3a578c203130
Tim <tima2508@yandex.ru>
2023-08-21 18:33:33 +0300

    bubble sort

--- problems/bubble-sort/bubble-sort.js
+++ problems/bubble-sort/bubble-sort.js
@@ -2,6 +2,19 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 export function bubbleSort(arr) {
+  let iter = arr.length;
 
-    return arr;
+  while (iter > 0) {
+    let pointer = 0;
+
+    for (let pointer = 0; pointer <= iter; pointer++) {
+      if (arr[pointer] > arr[pointer + 1]) {
+        [arr[pointer], arr[pointer + 1]] = [arr[pointer + 1], arr[pointer]];
+      }
+    }
+
+    iter--;
+  }
+
+  return arr;
 }
