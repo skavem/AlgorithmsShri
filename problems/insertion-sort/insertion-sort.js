8b4eda430e473f592211c90968416322491e7778
Tim <tima2508@yandex.ru>
2023-08-21 19:17:37 +0300

    insertion-sert

--- problems/insertion-sort/insertion-sort.js
+++ problems/insertion-sort/insertion-sort.js
@@ -2,6 +2,15 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 export function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] >= arr[j - 1]) {
+        break
+      }
 
-    return arr;
+      [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+    }
+  }
+
+  return arr;
 }
