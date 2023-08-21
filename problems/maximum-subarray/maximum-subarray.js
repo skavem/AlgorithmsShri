556805e925ee8bc619219ec4f3e6e7b3539c5873
Tim <tima2508@yandex.ru>
2023-08-21 21:56:47 +0300

    maximum-subarray

--- problems/maximum-subarray/maximum-subarray.js
+++ problems/maximum-subarray/maximum-subarray.js
@@ -1,5 +1,12 @@
 // https://leetcode.com/problems/maximum-subarray/
 
 export function maximumSubarray(arr) {
+  const dp = []
 
+  dp[0] = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    dp[i] = (dp[i-1] > 0 ? dp[i-1] : 0) + arr[i];
+  }
+
+  return Math.max(...dp)
 }
