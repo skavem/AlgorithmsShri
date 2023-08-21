4709362d599c8ff83e6b6c9882cf86d2a2ee5a35
Tim <tima2508@yandex.ru>
2023-08-21 19:54:46 +0300

    valid-parenthesis

--- problems/valid-parenthesis/valid-parenthesis.js
+++ problems/valid-parenthesis/valid-parenthesis.js
@@ -1,5 +1,28 @@
 // https://leetcode.com/problems/valid-parentheses/
 
-export function isValid(str) {
+const closings = {
+  ")": "(",
+  "]": "[",
+  "}": "{",
+};
 
+const openings = new Set(['(', '[', '{'])
+
+export function isValid(str) {
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const ch = str[i]
+
+    if (openings.has(ch)) {
+      stack.push(ch)
+      continue
+    }
+
+    if (closings[str[i]] !== stack.pop()) {
+      return false
+    }
+  }
+
+  return true
 }
