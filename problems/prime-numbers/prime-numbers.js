120fba78977348b9a59b9d7c6942be432f678848
Tim <tima2508@yandex.ru>
2023-08-21 20:28:16 +0300

    prime-numbers

--- problems/prime-numbers/prime-numbers.js
+++ problems/prime-numbers/prime-numbers.js
@@ -1,7 +1,21 @@
 // https://ru.wikipedia.org/wiki/Решето_Эратосфена
 
 export function primeNumbers(num) {
-    const result = [];
+  const result = [2];
+  const sqrt = Math.sqrt(num) | 0;
 
-    return result;
+  const visited = new Set();
+
+  for (let i = 2; i <= num; i++) {
+    if (visited.has(i)) continue;
+    visited.add(i)
+
+    for (let j = i; j <= num; j += i) {
+      visited.add(j);
+    }
+
+    result.push(i);
+  }
+
+  return result;
 }
